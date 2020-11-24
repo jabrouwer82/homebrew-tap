7637c1b37b9990bbee5cb9ecbe76369a96deb376
Jacob Brouwer <jabrouwer82@gmail.com>
2020-11-24 17:17:42 -0600

    Split ammonite by scala version.

--- Formula/amm213.rb
+++ Formula/amm213.rb
@@ -1,23 +1,22 @@
-class AmmRepl < Formula
+class Amm213 < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://ammonite.io/"
   url "https://github.com/lihaoyi/Ammonite/releases/download/2.2.0/2.13-2.2.0"
   sha256 "291c21d9839c1e95cb03d06504ace473da2cc1d3906dc9b7595aa061a1b28b80"
   license "MIT"
-  version "2.13-2.20"
 
   bottle :unneeded
 
   depends_on "openjdk"
 
   def install
-    (libexec/"bin").install Dir["*"].first => "amm"
-    chmod 0755, libexec/"bin/amm"
-    (bin/"amm").write_env_script libexec/"bin/amm", Language::Java.overridable_java_home_env
+    (libexec/"bin").install Dir["*"].first => "amm213"
+    chmod 0755, libexec/"bin/amm213"
+    (bin/"amm213").write_env_script libexec/"bin/amm213", Language::Java.overridable_java_home_env
   end
 
   test do
-    output = shell_output("#{bin}/amm -c 'print(\"hello world!\")'")
+    output = shell_output("#{bin}/amm213 -c 'print(\"hello world!\")'")
     assert_equal "hello world!", output.lines.last
   end
 end
